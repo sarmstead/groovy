0ee26cc51fae36c0dc8a7660394adc2beaea4426
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-18 18:01:59 -0400

    Create current year shortcode function

--- functions.php
+++ functions.php
@@ -5,3 +5,13 @@ function groovy_enqueue_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'groovy_enqueue_scripts' );
+
+// Create current year shortcode function
+function groovy_get_current_year() {
+	// Return current year in a shortcode
+	add_shortcode( 'groovy_current_year', function( $atts = null, $content = null, $shortcode_tag = 'groovy_current_year' ) {
+		return date("Y");
+	} );
+}
+
+add_action( 'init', 'groovy_get_current_year' );
